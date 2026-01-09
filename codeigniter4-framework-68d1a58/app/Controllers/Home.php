fa1a42203f889644990b7b5286ee77fcbedc6f24
Kéwan B <mail@kewan.fr>
2026-01-09 07:25:00 +0100

    Controllers home, search category

--- codeigniter4-framework-68d1a58/app/Controllers/Home.php
+++ codeigniter4-framework-68d1a58/app/Controllers/Home.php
@@ -12,8 +12,27 @@ class Home extends BaseController
     public function index(): string
     {
         $model = new ProductModel();
-
-        $data["products"] = $model->getAllProducts();
+        
+        // Récupère les paramètres de recherche et filtres
+        $search = $this->request->getGet('search');
+        $category = $this->request->getGet('category');
+        $tag = $this->request->getGet('tag');
+        
+        // Filtre les produits
+        if ($search || $category || $tag) {
+            $products = $model->searchAndFilter($search, $category, $tag);
+        } else {
+            $products = $model->getAllProducts();
+        }
+        
+        $data = [
+            "products" => $products,
+            "categories" => $model->getCategories(),
+            "tags" => $model->getAllTags(),
+            "currentSearch" => $search,
+            "currentCategory" => $category,
+            "currentTag" => $tag
+        ];
 
         return view('layout/main', $data);
     }
