5cfdd19d26913d7e4c619ca25b80021733499631
Kéwan <kewan.bernier@etu.univ-nantes.fr>
2026-01-16 11:11:51 +0000

    change BaseURL, allow localhost

--- codeigniter4-framework-68d1a58/app/Config/App.php
+++ codeigniter4-framework-68d1a58/app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'https://technopomme.kewan.fr/';
+    public string $baseURL = '/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
@@ -29,7 +29,7 @@ class App extends BaseConfig
      *
      * @var list<string>
      */
-    public array $allowedHostnames = ['localhost', '127.0.0.1', 'sae.home.kewan.fr', 'technopomme.kewan.fr', 'pommehub.kewan.fr', 'pommehub.kewan.dev'];
+    public array $allowedHostnames = ['localhost', 'localhost:8080', '127.0.0.1', 'sae.home.kewan.fr', 'technopomme.kewan.fr', 'pommehub.kewan.fr', 'pommehub.kewan.dev'];
 
     /**
      * --------------------------------------------------------------------------
