e6d6413537ed4dbff0d25cca05375c7de3307b00
Kéwan B <mail@kewan.fr>
2026-01-16 19:04:38 +0100

    fix error

--- codeigniter4-framework-68d1a58/app/Models/Users.php
+++ codeigniter4-framework-68d1a58/app/Models/Users.php
@@ -15,7 +15,7 @@ class Users extends UserModel
      * @param $email
      * @return array|object|null
      */
-    public function getUserByEmail($email) {x
+    public function getUserByEmail($email) {
         return $this->select('users.*')
             ->where('users.email', $email)
             ->first();
